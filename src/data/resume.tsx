a85fbb31cd80b847037e55430c85c6000ce4bc9d
srini41 <79924145+srini41@users.noreply.github.com>
2024-07-31 15:25:26 -0400

    stupid change

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -2,14 +2,15 @@ import { Icons } from "@/components/icons";
 import { CodeIcon, HomeIcon, NotebookIcon, PencilLine } from "lucide-react";
 
 export const DATA = {
-  name: "Srinikesh Singarapu",
-  initials: "SS",
-  url: "https://www.youtube.com/watch?v=eLmpKKaQL54",
-  location: "https://neetcode.io/courses/dsa-for-beginners/26",
+  name: "Dillion Verma",
+  initials: "DV",
+  url: "https://dillion.io",
+  location: "San Francisco, CA",
+  locationLink: "https://www.google.com/maps/place/sanfrancisco",
   description:
-    "Aspiring TPM. I love building things and helping people.",
+    "Software Engineer turned Entrepreneur. I love building things and helping people. Very active on Twitter.",
   summary:
-    "I love to fish",
+    "At the end of 2022, I quit my job as a software engineer to go fulltime into building and scaling my own SaaS businesses. In the past, [I pursued a double degree in computer science and business](/#education), [interned at big tech companies in Silicon Valley](https://www.youtube.com/watch?v=d-LJ2e5qKdE), and [competed in over 21 hackathons for fun](/#hackathons). I also had the pleasure of being a part of the first ever in-person cohort of buildspace called [buildspace sf1](https://buildspace.so/sf1).",
   avatarUrl: "/me.png",
   skills: [
     "React",
